aff5a6a0964cabc24dc50a64d0d3337d93fbc9a3
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-12-07 13:01:29 +0000

    test: Test data
    Added refdata test data, and enriched mockGenerators data
    
    SI-3

---
+++ test/jest/refdata.js
@@ -0,0 +1,68 @@
+const refdata = [
+  {
+    id: 'b764b4c3658a321c0eecd3c12f187222',
+    desc: 'DashboardAccess.Access',
+    internal: true,
+    values: [
+      {
+        id: 'ff80818184ec8e940184ec8f22620008',
+        value: 'edit',
+        label: 'Edit'
+      },
+      {
+        id: 'ff80818184ec8e940184ec8f226b0009',
+        value: 'view',
+        label: 'View'
+      },
+      {
+        id: '52881ea1f688714b6d6e362924376002',
+        value: 'manage',
+        label: 'Manage'
+      }
+    ]
+  },
+  {
+    id: 'ff80818184ec8e940184ec8f21e40000',
+    desc: 'NumberGeneratorSequence.CheckDigitAlgo',
+    internal: false,
+    values: [
+      {
+        id: 'ff80818184ec8e940184ec8f22070001',
+        value: 'none',
+        label: 'None'
+      },
+      {
+        id: 'ff80818184ec8e940184ec8f22310005',
+        value: 'modulo16',
+        label: 'Modulo16'
+      },
+      {
+        id: 'ff80818184ec8e940184ec8f223b0006',
+        value: 'modulo43',
+        label: 'Modulo43'
+      },
+      {
+        id: 'ff80818184ec8e940184ec8f221d0003',
+        value: 'modulo10',
+        label: 'Modulo10'
+      },
+      {
+        id: 'ff80818184ec8e940184ec8f22270004',
+        value: 'modulo11',
+        label: 'Modulo11'
+      },
+      {
+        id: 'ff80818184ec8e940184ec8f22450007',
+        value: 'modulo47',
+        label: 'Modulo47'
+      },
+      {
+        id: 'ff80818184ec8e940184ec8f22130002',
+        value: 'ean13',
+        label: 'EAN13'
+      }
+    ]
+  }
+];
+
+export default refdata;
