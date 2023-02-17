18c233e889f6af55f8ee89985706edd109f03259
Ethan Freestone <ethan.freestone@k-int.co.uk>
2023-02-17 16:41:50 +0000

    test: useSIRefdata
    Useless test for useSIRefdata, only serves to bump test coverage, actually tests nothing (Since it's only really caling useRefdata, which should be tested in kint-components)

---
+++ src/hooks/useSIRefdata.test.js
@@ -0,0 +1,53 @@
+import { render } from '@testing-library/react';
+
+import useSIRefdata from './useSIRefdata';
+import mockRefdata from '../../test/jest/refdata';
+
+const TestComponent = ({ returnQueryObject }) => {
+  useSIRefdata({ returnQueryObject });
+  return <div> Component </div>;
+};
+
+
+jest.mock('@k-int/stripes-kint-components', () => ({
+  useRefdata: jest.fn(({ returnQueryObject = false }) => {
+    if (!returnQueryObject) {
+      return mockRefdata;
+    }
+
+    return ({
+      data: mockRefdata
+    });
+  }),
+  refdataOptions: {}
+}));
+
+// This test is FULLY useless... only serves to get coverage up
+describe('useSIRefdata', () => {
+  let renderComponent;
+  describe('useSIRefdata', () => {
+    beforeEach(() => {
+      renderComponent = render(
+        <TestComponent />
+      );
+    });
+
+    test('component renders without failure', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Component')).toBeInTheDocument();
+    });
+  });
+
+  describe('useSIRefdata with returnQueryObject', () => {
+    beforeEach(() => {
+      renderComponent = render(
+        <TestComponent returnQueryObject />
+      );
+    });
+
+    test('component renders without failure', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Component')).toBeInTheDocument();
+    });
+  });
+});
