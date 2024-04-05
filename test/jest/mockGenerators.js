9f0a8ffd816e980a2923be4deb6f2dc581da79b2
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2024-04-05 14:13:48 +0100

    test: Test coverage for NumberGeneratorSelector (#83)
    Messive boost to coverage for NumberGeneratorSelector component, providing an example of how one might test a QueryTypedown (Or plain Typedown) component's utility as part of a larger suite of tests.

--- test/jest/mockGenerators.js
+++ test/jest/mockGenerators.js
@@ -11,6 +11,16 @@ const getCheckDigitAlgoByValue = (val) => {
   };
 };
 
+const getMaximumCheckByValue = (val) => {
+  const { values, ...rdcRest } = refdata?.find(rdc => rdc?.desc === 'NumberGeneratorSequence.MaximumCheck');
+  const value = values?.find(rdv => rdv?.value === val);
+
+  return {
+    ...value,
+    owner: rdcRest
+  };
+};
+
 const numberGenerator1 = {
   id: 'number-generator-1',
   code: 'numberGen1',
@@ -99,7 +109,83 @@ const numberGenerator2 = {
   ]
 };
 
+const numberGenerator3 = {
+  id: '30f48059-b0b9-4434-8550-1aa12ea9b00f',
+  sequences: [
+    {
+      id: '352d11b6-095b-44d6-a053-b733a9c7510a',
+      code: 'b_test',
+      nextValue: 1,
+      name: 'B. Test',
+      checkDigitAlgo: getCheckDigitAlgoByValue('none'),
+      enabled: true
+    },
+    {
+      id: 'aa0bf779-3622-4bae-8203-832824f599f8',
+      code: 'a_test',
+      nextValue: 1,
+      name: 'A. Test',
+      checkDigitAlgo: getCheckDigitAlgoByValue('none'),
+      enabled: true
+    },
+    {
+      id: '559e4451-7209-4353-9ce1-6cc9de85d026',
+      code: 'efTest3',
+      nextValue: 101,
+      format: '###',
+      name: 'EF Test 3',
+      maximumNumber: 100,
+      maximumCheck: getMaximumCheckByValue('at_maximum'),
+      checkDigitAlgo: getCheckDigitAlgoByValue('none'),
+      enabled: true
+    },
+    {
+      id: 'fb15ef2b-76c8-47ef-826d-f7cbb763d34a',
+      code: 'a_2_test',
+      nextValue: 1,
+      name: 'a. test',
+      checkDigitAlgo: getCheckDigitAlgoByValue('none'),
+      enabled: false
+    },
+    {
+      id: '8c2bca3e-f677-4a9e-88e7-312eb8dd48dc',
+      code: 'eftest1',
+      nextValue: 50,
+      maximumNumberThreshold: 95,
+      format: '####',
+      name: 'EF Test 1',
+      maximumNumber: 100,
+      maximumCheck: getMaximumCheckByValue('below_threshold'),
+      checkDigitAlgo: getCheckDigitAlgoByValue('none'),
+      enabled: true
+    },
+    {
+      id: '42d5c637-6294-4f76-9e97-9c5f3c787834',
+      code: 'efTest2',
+      nextValue: 95,
+      maximumNumberThreshold: 90,
+      format: '####',
+      name: 'EF Test 2',
+      maximumNumber: 100,
+      maximumCheck: getMaximumCheckByValue('over_threshold'),
+      checkDigitAlgo: getCheckDigitAlgoByValue('none'),
+      enabled: true
+    },
+    {
+      id: '26b4378e-52d1-474d-b5d3-15af82ccd65d',
+      code: 'b_2_test',
+      nextValue: 1,
+      name: 'b. test',
+      checkDigitAlgo: getCheckDigitAlgoByValue('none'),
+      enabled: true
+    }
+  ],
+  code: 'ef_test_gen',
+  name: 'EF Test Gen'
+};
+
 export {
   numberGenerator1,
-  numberGenerator2
+  numberGenerator2,
+  numberGenerator3
 };
